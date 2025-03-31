f957a3ee4d54047731068435e5842d684e43e601
JarikiSungnyung <hyuk0496@gmail.com>
2025-03-31 12:21:17 +0900

    Solve: BOJ 10896

---
+++ Codes/10869.py
@@ -0,0 +1,7 @@
+input = input()
+num = input.split()
+print(int(num[0]) + int(num[1]))
+print(int(num[0]) - int(num[1]))
+print(int(num[0]) * int(num[1]))
+print(int(num[0]) // int(num[1]))
+print(int(num[0]) % int(num[1]))
